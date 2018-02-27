e98d7c97750e3e159d2d314ff49d217498b1d2f4
root <root@localhost.localdomain>
2018-02-27 17:33:31 +0300

    no wait

--- mininet-qnet-tap/tests.sh
+++ mininet-qnet-tap/tests.sh
@@ -3,4 +3,4 @@ now=$(date +"%H_%M_%d_%m_%Y")
 filename="./results/result_$now.log"
 echo "Testing time $(date)" | tee $filename
 echo "Saved to file $filename"
-(sleep 10; echo " out iperf3 -s & "; sleep 2; echo " in iperf3 -c out "; sleep 1; echo " quit ") | python mininet-qnet-tap.py defaults_1.yaml single-host-udp.yaml h1 2>&1 | tee -a $filename
+(echo " out iperf3 -s & "; sleep 2; echo " in iperf3 -c out "; sleep 1; echo " quit ") | stdbuf -o0 -e0 python mininet-qnet-tap.py defaults_1.yaml single-host-udp.yaml h1 2>&1 | tee -a $filename
